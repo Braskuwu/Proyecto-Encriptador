4ce7367e8c4767be324a51ada3542e8035b54d79
Brask <Dafribdfv@gmail.com>
2023-01-28 13:16:58 -0500

    Resuelta logica encriptador

--- componentes/script.js
+++ componentes/script.js
@@ -8,8 +8,20 @@ const mensaje = document.querySelector(".mensaje");
 // La letra "o" es convertida para "ober"
 // La letra "u" es convertida para "ufat"
 
-let matrizCodigo = 
+function btnEncriptar(){
+    const textoEncriptado = Encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+}
 
-function Encriptar (){
+function Encriptar (stringEncriptado){
 
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptado
 }
